c9595d037599ab09b15f961f76724ad03866c22d
Oyinbayode <oyinbayode75@gmail.com>
2022-05-23 15:25:12 +0100

    feat: responsiveness for ZX9 container finished

--- src/components/HomeMain/components/ZX9/ZX9.js
+++ src/components/HomeMain/components/ZX9/ZX9.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Zx9Container = styled.div`
-  border: 1px solid black;
+  border-radius: 8px;
   max-width: 1110px;
   width: 100%;
   background-color: #d87d4a;
@@ -9,11 +9,31 @@ export const Zx9Container = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   background-position: 0 0;
+  margin-left: 7vw;
+  margin-right: 7vw;
   /* background-attachment: fixed; */
 
   margin-bottom: 100px;
   display: flex;
   align-items: center;
+
+  @media screen and (max-width: 1090px) {
+    /* max-width: fit-content; */
+    /* margin-left: 1vw;
+    margin-right: 1vw; */
+  }
+
+  @media screen and (max-width: 1090px) {
+    background-image: url("./images/mobile-oval-bg.svg");
+    background-size: contain;
+    max-width: 689px;
+  }
+
+  @media screen and (max-width: 600px) {
+    background-image: url("./images/iphone-bg.svg");
+    background-size: contain;
+    max-width: fit-content;
+  }
 `;
 
 export const Frame = styled.div`
@@ -23,6 +43,14 @@ export const Frame = styled.div`
   margin-top: 133px;
   margin-left: 9vw;
   margin-right: 8vw;
+  flex-wrap: wrap;
+
+  @media screen and (max-width: 1090px) {
+    flex-direction: column;
+    align-items: center;
+    margin-top: 52px;
+    margin-bottom: 64px;
+  }
 `;
 
 export const Zx9Image = styled.div`
@@ -32,7 +60,13 @@ export const Zx9Image = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid black;
+  max-width: 400px;
+
+  @media screen and (max-width: 1090px) {
+    max-width: 197px;
+    align-self: center;
+    margin-bottom: 64px;
+  }
 `;
 
 export const Zx9Img = styled.img`
@@ -45,8 +79,12 @@ export const Zx9Body = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  border: 1px solid black;
   height: 303px;
+
+  @media screen and (max-width: 1090px) {
+    text-align: center;
+    align-items: center;
+  }
 `;
 
 export const Zx9Header = styled.h1`
@@ -58,6 +96,14 @@ export const Zx9Header = styled.h1`
   letter-spacing: 2px;
   text-transform: uppercase;
   margin: 0;
+
+  @media screen and (max-width: 500px) {
+    font-size: 36px;
+    line-height: 40px;
+    letter-spacing: 1.28571px;
+    max-width: 200px;
+    text-align: center;
+  }
 `;
 
 export const Zx9Text = styled.p`
@@ -67,4 +113,10 @@ export const Zx9Text = styled.p`
   color: #fff;
   opacity: 0.75;
   margin-bottom: 40px;
+
+  @media screen and (max-width: 500px) {
+    margin-bottom: 14px;
+    max-width: 280px;
+    width: 100%;
+  }
 `;
